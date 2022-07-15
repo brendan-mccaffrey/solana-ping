1cc9daf5f7720d23c64af26abb8133f2436ea2dd
brendan <brendanchristophermccaffrey@gmail.com>
2022-07-14 23:18:06 -0400

    almost done with starter solana app but i have one error

---
+++ programs/solana-ping/src/lib.rs
@@ -0,0 +1,45 @@
+use anchor_lang::prelude::*;
+
+declare_id!("FA51D5fh1VhruC5ZhG2MVvRjfS233QnSD5yEPCbYFd3U");
+
+#[program]
+pub mod solana_ping {
+    use anchor_lang::solana_program::entrypoint::ProgramResult;
+
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let base_account = &mut ctx.accounts.base_account;
+        base_account.count += 1;
+        Ok(())
+    }
+}
+
+// tx instructions
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 16 + 16)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+// tx instructions
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+}
+
+// An account that is passed to tx instruction
+#[account]
+pub struct BaseAccount {
+    pub count: u64,
+}
---
+++ tests/solana-ping.ts
@@ -0,0 +1,62 @@
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
+import assert = require("assert");
+import { createFreezeAccountInstruction } from "@solana/spl-token";
+import { SolanaPing } from "../target/types/solana_ping";
+import { SystemProgram } from "@solana/web3.js";
+
+describe("solana-ping", () => {
+    // Configure the client to use the local cluster.
+
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
+
+    const program = anchor.workspace.SolanaPing; // as Program<SolanaPing>;
+
+    let _baseAccount;
+
+    it("Creates a counter:", async () => {
+
+        // create account
+        let baseAccount = await anchor.web3.Keypair.generate();
+
+        console.log("Geneerated keypair with pubkey: ", baseAccount.publicKey.toString())
+
+        // initialize account
+        const tx = await program.rpc.initialize({
+            accounts: {
+                baseAccount: baseAccount.publicKey,
+                user: provider.wallet.publicKey,
+                systemProgram: SystemProgram.programId,
+            },
+            signers: [baseAccount],
+        });
+
+        console.log("TX Result: ", tx)
+
+        // Fetch account and check value of count
+        const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+        console.log("Count should be 0 here: ", account.count.toString())
+        assert.ok(account.count.toString() == 0);
+
+        _baseAccount = baseAccount;
+    });
+
+    it("Increments the counter:", async () => {
+
+        // create account
+        const baseAccount = _baseAccount;
+
+        // call increment
+        await program.rpc.increment({
+            accounts: {
+                baseAccount: baseAccount.publicKey,
+            },
+        });
+
+        // Fetch account and check if incremented to 1
+        const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+        console.log("Count should be 1 here: ", account.count.toString())
+        assert.ok(account.count.toString() == 1);
+    });
+});
